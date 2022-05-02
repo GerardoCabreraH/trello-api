736756315d8a4c1291db2ac1563ace73a5f491e1
Gerardo Rafael Cabrera Herrera <gecabrera@outlook.com>
2022-05-02 16:47:13 -0500

    Hacer funcionar trello

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-require("dotenv").config();
+require('dotenv').config()
 
-if (!process.env.TOKEN && !process.env.KEY) {
-  throw new Error("No hay configuración con Api Key y con Token");
+if(!process.env.TOKEN && !process.env.KEY){
+    throw new Error('No hay configuración con Api Key y con Token')
 }
+
+let Trello = require("trello");
+let trello = new Trello(process.env.KEY, process.env.TOKEN);
+
+let cardTitle = `Card Nueva ${new Date()}`
+
+trello.addCard(cardTitle, "LaunchX Card Description", "627014b6b6da99245b02e677",
+function (error, trelloCard) {
+    if (error) {
+        console.log('Could not add card:', error);
+    }
+    else {
+        console.log('Added card:', trelloCard);
+    }
+});
