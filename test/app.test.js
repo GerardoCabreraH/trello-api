fedb77386a2987778d26e5b7169e5ade08af800b
Gerardo Rafael Cabrera Herrera <gecabrera@outlook.com>
2022-05-02 22:53:15 -0500

    Pruebas de la dependencia Trello

---
+++ test/app.test.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+if (!process.env.TOKEN && !process.env.KEY) {
+  throw new Error("No hay configuración con Api Key y con Token");
+}
+
+const Trello = require("trello");
+
+const trello = new Trello(process.env.KEY, process.env.TOKEN);
+
+describe("Probar Trello", () => { 
+    test("Probar dependencia Trello", () => {
+        const cardTitle = `Card Nueva ${new Date()}`;
+        trello.addCard(cardTitle, "LaunchX Card Description", "627014b6b6da99245b02e677",
+        function (error, trelloCard) {
+            if (error) {
+                expect(error).toBe(error);
+            }
+            else {
+                expect(trelloCard.desc).toBe("LaunchX Card Description");
+            }
+            }
+        );
+    });
+});
